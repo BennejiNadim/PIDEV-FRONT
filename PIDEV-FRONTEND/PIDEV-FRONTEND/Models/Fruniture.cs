3d7bd6d5a6ee251102a2184ab3a56c79207ad769
iifast2 <mohamedamine.bentaieb@esprit.tn>
2021-04-28 14:15:25 +0100

    fixing some errors

---
+++ PIDEV-FRONTEND/PIDEV-FRONTEND/Models/Fruniture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PIDEV_FRONTEND.Models
+{
+    public class Fruniture
+    {
+
+        //   public int furnitureId { get; set; }
+        private String FurnitureName { get; set; }
+        private String fabricator { get; set; }
+        private String category { get; set; }
+        private double shippingPrice { get; set; }
+        private int numberOfPieces { get; set; }
+        private double FurniturePrice { get; set; }
+        private Boolean verified { get; set; }
+        private Boolean active { get; set; }
+
+
+
+    }
+}
+
