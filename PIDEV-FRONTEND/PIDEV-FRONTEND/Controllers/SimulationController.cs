7aebaf94bb136a334a401af2413496df72448482
mohamedkhammeri <mohamed.khammeri@esprit.tn>
2021-04-29 06:12:56 +0100

    add simulation

---
+++ PIDEV-FRONTEND/PIDEV-FRONTEND/Controllers/SimulationController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace PIDEV_FRONTEND.Controllers
+{
+    public class SimulationController : Controller
+    {
+        // GET: Simulation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
