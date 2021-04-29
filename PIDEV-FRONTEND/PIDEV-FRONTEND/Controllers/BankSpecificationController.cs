e58da22f9f784d537d39a4bad6e4127a632aea95
mohamedkhammeri <mohamed.khammeri@esprit.tn>
2021-04-29 06:12:46 +0100

    add bankSpecification

---
+++ PIDEV-FRONTEND/PIDEV-FRONTEND/Controllers/BankSpecificationController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using PIDEV_FRONTEND.Models;
+
+namespace PIDEV_FRONTEND.Controllers
+{
+    public class BankSpecificationController : Controller
+    {
+        // GET: BankSpecification
+        public ActionResult Index()
+        {
+            IEnumerable<BankSpecification> result = WebApiHelper.Get<IEnumerable<BankSpecification>>("bank/");
+
+            return View(result);
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Create(BankSpecification bs)
+        {
+            WebApiHelper.Post<BankSpecification>("bank/", bs);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(long id)
+        {
+
+            BankSpecification bs = WebApiHelper.Get<BankSpecification>("bank/" + id.ToString());
+
+            return View(bs);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(BankSpecification bs)
+        {
+            WebApiHelper.Put<BankSpecification>("bank/", bs);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(long id)
+        {
+            WebApiHelper.Delete<object>("bank/" + id.ToString());
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult Details(long id)
+        {
+
+            BankSpecification bs = WebApiHelper.Get<BankSpecification>("bank/" + id.ToString());
+
+            return View(bs);
+
+        }
+    }
+}
---
+++ PIDEV-FRONTEND/PIDEV-FRONTEND/Models/BankSpecification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PIDEV_FRONTEND.Models
+{
+    public class BankSpecification
+    {
+        public long id { get; set; }
+        public string bankName { get; set; }
+        public string preferences { get; set; }
+        public long value { get; set; }
+    }
+}
