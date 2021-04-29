7a24c2a0c75636fcbb7eb933167eea03939eeec5
iifast2 <mohamedamine.bentaieb@esprit.tn>
2021-04-29 09:47:52 +0100

    furn

--- PIDEV-FRONTEND/PIDEV-FRONTEND/Controllers/FurnitureController.cs
+++ PIDEV-FRONTEND/PIDEV-FRONTEND/Controllers/FurnitureController.cs
@@ -44,9 +44,54 @@ namespace PIDEV_FRONTEND.Controllers
         }
 
 
-    }
+
+
+
+
+        //// adding Furniture
+        //public async System.Threading.Tasks.Task<ActionResult> AddFurnitureAsync(string furnitureName, string fabricator, double shippingPrice)
+        //{
+        //    Furniture furn = new Furniture();
+        //    furn.furnitureName = furnitureName;
+        //    furn.fabricator = fabricator;
+        //    furn.shippingPrice = shippingPrice;
+
+
+        //    HttpClient client = new HttpClient();
+        //    client.BaseAddress = new Uri("http://localhost:8081");
+        //    string jwtEncodedString = Request.Cookies["Token"].Value;
+        //  //  Debug.WriteLine(jwtEncodedString);
+        //    // var token = new JwtSecurityToken(jwtEncodedString: jwtEncodedString);
+
+        //    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtEncodedString);
+        //    HttpResponseMessage response = await client.PostAsJsonAsync<Furniture>("api/addFurniture", furn).ContinueWith((postTask) => postTask.Result);
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        ViewBag.status = response.StatusCode;
+        //        ViewBag.auth = client.DefaultRequestHeaders.Authorization;
+        //    }
+        //    else
+        //    {
+        //        ViewBag.status = response.StatusCode;
+        //        ViewBag.auth = client.DefaultRequestHeaders.Authorization;
+        //        //Debug.WriteLine(response.ToString());
+        //        //Debug.WriteLine(client.DefaultRequestHeaders.Authorization);
+        //        //Debug.WriteLine(furnitureName);
+        //        //Debug.WriteLine(fabricator);
+        //        //Debug.WriteLine(shippingPrice);
+        //    }
+        //    return View("home");
+        //}
+
+
+
+
+
+
 
     }
+
+}
 
 
        
