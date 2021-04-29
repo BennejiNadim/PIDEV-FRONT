9b5d12e0580d20f00702317b590a49a965e17c27
mohamedkhammeri <mohamed.khammeri@esprit.tn>
2021-04-29 05:08:11 +0100

    add api helper

---
+++ PIDEV-FRONTEND/PIDEV-FRONTEND/Shared/WebApiHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace PIDEV_FRONTEND
+{
+    public static class WebApiHelper
+    {
+
+        private static HttpClient getHttpClient()
+        {
+            var httpClient = new HttpClient();
+
+            httpClient.BaseAddress = new Uri("http://localhost:8081");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return httpClient;
+        }
+
+        public static T Get<T>(string api, Action<HttpClient> beforeGet = null)
+        {
+            using (HttpClient httpClient = getHttpClient())
+            {
+                if (beforeGet != null)
+                {
+                    beforeGet(httpClient);
+                }
+
+                var response = httpClient.GetAsync(api).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return response.Content.ReadAsAsync<T>().Result;
+                }
+            }
+
+            return default;
+        }
+
+        public static T Post<T>(string api, T payload)
+        {
+            using (HttpClient httpClient = getHttpClient())
+            {
+
+                var response = httpClient.PostAsJsonAsync(api, payload)
+                    .ContinueWith((postTask) => postTask.Result.EnsureSuccessStatusCode()).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return response.Content.ReadAsAsync<T>().Result;
+                }
+            }
+
+            return default;
+        }
+
+        public static T Put<T>(string api, T payload)
+        {
+            using (HttpClient httpClient = getHttpClient())
+            {
+
+                var response = httpClient.PutAsJsonAsync(api, payload)
+                    .ContinueWith((postTask) => postTask.Result.EnsureSuccessStatusCode()).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return response.Content.ReadAsAsync<T>().Result;
+                }
+            }
+
+            return default;
+        }
+
+        public static T Delete<T>(string api)
+        {
+            using (HttpClient httpClient = getHttpClient())
+            {
+
+                var response = httpClient.DeleteAsync(api)
+                    .ContinueWith((postTask) => postTask.Result.EnsureSuccessStatusCode()).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return response.Content.ReadAsAsync<T>().Result;
+                }
+            }
+
+            return default;
+        }
+    }
+}
